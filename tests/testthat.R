d719ebaf6bc90fb30370ff7fb725e10557a85a54
Alan Dipert <alan@dipert.org>
2020-04-21 17:25:50 +0000

    Simplify testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,11 @@
 library(testthat)
 library(shiny)
 
-e1 <- new.env(parent = globalenv())
-e2 <- new.env(parent = e1)
-loadSupport(renv = e2, globalrenv = e1)
+parent <- new.env(parent = globalenv())
+env <- loadSupport(renv = new.env(parent = parent), globalrenv = parent)
 
 testthat::test_dir(
   "./testthat",
-  reporter = SummaryReporter,
-  env = e2,
-  stop_on_failure = TRUE
+  reporter = c("summary", "fail"),
+  env = env
 )
